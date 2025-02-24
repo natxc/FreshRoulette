995c0478dff7797c7d0dc72ddcfef6b82a351b0d
Natalie <natalie.r.crow@gmail.com>
2025-02-23 18:16:30 -0700

    homepage refresh

--- client/src/Pages/Home/App.test.js
+++ client/src/Pages/Home/App.test.js
@@ -5,5 +5,5 @@ import "@testing-library/jest-dom";
 
 test("renders App component", () => {
   render(<App />);
-  expect(screen.getByText(/what's cookin'/i)).toBeInTheDocument();
+  expect(screen.getByText(/what's on the menu'/i)).toBeInTheDocument();
 });
